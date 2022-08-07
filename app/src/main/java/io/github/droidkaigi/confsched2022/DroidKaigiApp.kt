2216a02df433fce7883d6527d853b3cbfc3f3746
takahirom <takam.dev@gmail.com>
2022-08-07 21:08:02 +0900

    Fix format

--- app/src/main/java/io/github/droidkaigi/confsched2022/DroidKaigiApp.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2022/DroidKaigiApp.kt
@@ -2,7 +2,6 @@ package io.github.droidkaigi.confsched2022
 
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
--- app/src/main/java/io/github/droidkaigi/confsched2022/DroidKaigiApplication.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2022/DroidKaigiApplication.kt
@@ -1,9 +1,7 @@
 package io.github.droidkaigi.confsched2022
 
 import android.app.Application
-import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DroidKaigiApplication : Application() {
-}
+class DroidKaigiApplication : Application()
