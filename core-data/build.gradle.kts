608375f7de0fe0ae9f268242bee8689c3050cfff
takahirom <takam.dev@gmail.com>
2022-08-27 15:20:09 +0900

    Fix auth build

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -25,6 +25,7 @@ kotlin {
                 implementation(libs.ktorClientOkHttp)
                 implementation(libs.androidxDatastorePreferences)
                 implementation(libs.multiplatformSettingsDatastore)
+                implementation(libs.firebaseCommon)
             }
         }
         val iosMain by getting {
