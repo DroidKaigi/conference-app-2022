899e353eb586b829361383439e36399e29dcc14a
sobaya-0141 <soba.ha.kenkou@gmail.com>
2022-08-28 23:10:53 +0900

    remove ifNotNull fun

--- core-ui/src/main/java/io/github/droidkaigi/confsched2022/ui/IfModifier.kt
+++ core-ui/src/main/java/io/github/droidkaigi/confsched2022/ui/IfModifier.kt
@@ -2,10 +2,6 @@ package io.github.droidkaigi.confsched2022.ui
 
 import androidx.compose.ui.Modifier
 
-inline fun <T : Any> Modifier.ifNotNull(value: T?, builder: (T) -> Modifier): Modifier {
-    return then(if (value != null) builder(value) else Modifier)
-}
-
 inline fun Modifier.ifTrue(value: Boolean, builder: () -> Modifier): Modifier {
     return then(if (value) builder() else Modifier)
 }
