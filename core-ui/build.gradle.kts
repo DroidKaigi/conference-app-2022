f483c9380383802e38e1ac33c23b0bd5acb61cab
sobaya-0141 <soba.ha.kenkou@gmail.com>
2022-09-02 21:10:59 +0900

    add paparazzi & showcase

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -1,3 +1,5 @@
+// TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("droidkaigi.primitive.android")
     id("droidkaigi.primitive.android.kotlin")
@@ -5,10 +7,13 @@ plugins {
     id("droidkaigi.primitive.android.hilt")
     id("droidkaigi.primitive.molecule")
     id("droidkaigi.primitive.spotless")
+    alias(libs.plugins.ksp)
 }
 
 android.namespace = "io.github.droidkaigi.confsched2022.template.core.ui"
 
 dependencies {
     implementation(libs.accompanistPager)
+    implementation(libs.showkase.runtime)
+    ksp(libs.showkase.processor)
 }
--- feature-contributors/build.gradle.kts
+++ feature-contributors/build.gradle.kts
@@ -1,5 +1,8 @@
+// TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("droidkaigi.convention.androidfeature")
+    alias(libs.plugins.ksp)
 }
 
 android.namespace = "io.github.droidkaigi.confsched2022.feature.contributors"
@@ -23,4 +26,7 @@ dependencies {
     androidTestImplementation(libs.composeUiTestJunit4)
     debugImplementation(libs.composeUiTooling)
     debugImplementation(libs.composeUiTestManifest)
+
+    implementation(libs.showkase.runtime)
+    ksp(libs.showkase.processor)
 }
