ff49582673ef655b448abb9fd0c1a654e90a2d28
yurihondo <yuri.hondo.dev@gmail.com>
2022-09-03 20:12:11 +0900

    Minor change

--- core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/theme/KaigiScaffold.kt
+++ core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/theme/KaigiScaffold.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
