6a1712d10eb53996a82d89a12fb2c9d5f11bccfc
takahirom <takam.dev@gmail.com>
2022-09-03 20:13:27 +0900

    Fix test

--- feature-sessions/src/test/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionsScreenTest.kt
+++ feature-sessions/src/test/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionsScreenTest.kt
@@ -3,10 +3,10 @@ package io.github.droidkaigi.confsched2022.feature.sessions
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.droidkaigi.confsched2022.testing.RobotTestRule
-import javax.inject.Inject
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
@@ -46,12 +46,13 @@ class SessionsScreenTest {
         }
     }
 
-    @Test
-    fun canToggleFavorite() {
-        sessionScreenRobot(robotTestRule) {
-            clickFavoriteAt(0)
-            checkFavoritedAt(index = 0, isFavorited = true)
-            checkFavoriteIsSavedAt(0)
-        }
-    }
+// Currently, there are not favorite buttons in timetable
+//    @Test
+//    fun canToggleFavorite() {
+//        sessionScreenRobot(robotTestRule) {
+//            clickFavoriteAt(0)
+//            checkFavoritedAt(index = 0, isFavorited = true)
+//            checkFavoriteIsSavedAt(0)
+//        }
+//    }
 }
