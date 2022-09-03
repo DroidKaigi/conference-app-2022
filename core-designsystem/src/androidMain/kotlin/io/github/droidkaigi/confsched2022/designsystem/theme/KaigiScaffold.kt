ccd8bb17babdc4cde9c5af49456ecbf2dfe11ca9
yurihondo <yuri.hondo.dev@gmail.com>
2022-09-03 16:55:23 +0900

    Apply toolbar design
    Applied toolbar design.

--- core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/theme/KaigiScaffold.kt
+++ core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/theme/KaigiScaffold.kt
@@ -1,8 +1,13 @@
 package io.github.droidkaigi.confsched2022.designsystem.theme
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,9 +15,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallTopAppBar
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.droidkaigi.confsched2022.core.designsystem.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,7 +36,29 @@ fun KaigiScaffold(
         topBar = {
             SmallTopAppBar(
                 title = {
-                    Text("DroidKaigi")
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            modifier = Modifier.size(30.dp),
+                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_app),
+                            contentDescription = "logo in toolbar"
+                        )
+                        Spacer(modifier = Modifier.weight(1F))
+                        Icon(
+                            modifier = Modifier.size(17.5.dp),
+                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_search),
+                            contentDescription = "Search icon in toolbar"
+                        )
+                        Spacer(modifier = Modifier.width(30.5.dp))
+                        Icon(
+                            modifier = Modifier
+                                .width(18.dp)
+                                .height(20.dp),
+                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_today),
+                            contentDescription = "Search icon in toolbar"
+                        )
+                    }
                 },
                 navigationIcon = {
                     IconButton(
@@ -45,5 +77,16 @@ fun KaigiScaffold(
         ) {
             content()
         }
+    }
+}
+
+@Preview
+@Composable
+fun KaigiScaffoldPreview() {
+    KaigiTheme {
+        KaigiScaffold(
+            onNavigationIconClick = {},
+            content = {}
+        )
     }
 }
