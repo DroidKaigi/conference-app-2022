7003cc69b181c63634ea1189dedd3a7a8a9b62f2
takahirom <takam.dev@gmail.com>
2022-09-03 19:33:23 +0900

    Delete ja

--- core-resources/build.gradle.kts
+++ core-resources/build.gradle.kts
@@ -6,24 +6,9 @@ plugins {
 android.namespace = "io.github.droidkaigi.confsched2022.core.resources"
 
 kotlin {
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(libs.kotlinxCollectionsImmutable)
-                api(libs.kotlinxCoroutinesCore)
-                api(libs.kotlinxDatetime)
-                implementation(libs.kotlinSerializationJson)
-            }
-        }
-        val androidMain by getting {
-            dependencies{
-                implementation(libs.composeRuntime)
-            }
-        }
-    }
 }
 
 multiplatformResources {
     multiplatformResourcesPackage = "io.github.droidkaigi.confsched2022.core.resources"
-    iosBaseLocalizationRegion = "ja" // default "en"
+//    iosBaseLocalizationRegion = "ja" // default "en"
 }
