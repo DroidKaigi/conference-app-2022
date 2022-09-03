5ac37b95992e8d9f02dba0cbd9630f8f59ff36dd
ryoya ito <ry.itto.github@gmail.com>
2022-09-03 22:44:12 +0900

    refactor: timetable-view

---
+++ app-ios/Sources/TimetableFeature/Content/ListView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+internal struct TimetableListView: View {
+    internal var body: some View {
+        Text("TODO: TimetableListView")
+    }
+}
+
+#if DEBUG
+struct TimetableListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimetableListView()
+    }
+}
+#endif
