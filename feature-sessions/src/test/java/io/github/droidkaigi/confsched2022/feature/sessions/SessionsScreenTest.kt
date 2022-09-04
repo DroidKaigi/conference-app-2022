e85008ace5effe5802263f1672b1ee27df92b9e4
takahirom <takam.dev@gmail.com>
2022-09-04 13:17:03 +0900

    Format

--- feature-sessions/src/test/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionsScreenTest.kt
+++ feature-sessions/src/test/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionsScreenTest.kt
@@ -3,10 +3,10 @@ package io.github.droidkaigi.confsched2022.feature.sessions
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.droidkaigi.confsched2022.testing.RobotTestRule
+import javax.inject.Inject
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
@@ -14,7 +14,6 @@ class SessionsScreenTest {
 
     @get:Rule val robotTestRule = RobotTestRule(this)
     @Inject lateinit var sessionScreenRobot: SessionScreenRobot
-
 
     @Test
     fun visibleTimetable() {
