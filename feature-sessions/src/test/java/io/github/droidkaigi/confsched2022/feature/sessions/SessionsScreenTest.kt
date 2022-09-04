a93870579b4d0d02c6802bcaf48b8def30d96ff4
takahirom <takam.dev@gmail.com>
2022-09-04 17:02:10 +0900

    Fix test

--- feature-sessions/src/test/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionsScreenTest.kt
+++ feature-sessions/src/test/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionsScreenTest.kt
@@ -3,10 +3,10 @@ package io.github.droidkaigi.confsched2022.feature.sessions
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.droidkaigi.confsched2022.testing.RobotTestRule
-import javax.inject.Inject
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
@@ -63,5 +63,4 @@ class SessionsScreenTest {
 //            checkFavoriteIsSavedAt(0)
 //        }
 //    }
-    >>>>>>> origin/main
 }
