9d0f580e54fd844a4604ac0652d907d0d6523483
takahirom <takam.dev@gmail.com>
2022-09-04 18:39:45 +0900

    Use zipline

--- core-zipline/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/zipline/SessionsZipline.kt
+++ core-zipline/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/zipline/SessionsZipline.kt
@@ -6,9 +6,6 @@ import app.cash.zipline.Zipline
 import app.cash.zipline.loader.ZiplineLoader
 import co.touchlab.kermit.Logger
 import io.github.droidkaigi.confsched2022.model.DroidKaigiSchedule
-import java.util.concurrent.Executors
-import javax.inject.Inject
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,6 +15,9 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
+import java.util.concurrent.Executors
+import javax.inject.Inject
+import kotlin.coroutines.EmptyCoroutineContext
 
 class SessionsZipline @Inject constructor(
     context: Application,
@@ -26,7 +26,7 @@ class SessionsZipline @Inject constructor(
     private val executorService = Executors.newSingleThreadExecutor { Thread(it, "Zipline") }
     private val dispatcher = executorService.asCoroutineDispatcher()
 
-    private val manifestUrl = "http://10.0.2.2:8080/manifest.zipline.json"
+    private val manifestUrl = "https://droidkaigi.github.io/conference-app-2022/manifest.zipline.json"
 
     private val ziplineLoader = ZiplineLoader(
         context = context,
