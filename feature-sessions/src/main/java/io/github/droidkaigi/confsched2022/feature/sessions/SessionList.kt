33fafdcab2901e172726f03d7e11914174618574
miyazato-hayato1 <miyazato-hayato1@dmm.com>
2022-09-04 17:36:47 +0900

    Add SessionList

---
+++ feature-sessions/src/main/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionList.kt
@@ -0,0 +1,30 @@
+package io.github.droidkaigi.confsched2022.feature.sessions
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.droidkaigi.confsched2022.model.Timetable
+import io.github.droidkaigi.confsched2022.model.TimetableItem
+
+@Composable
+fun SessionList(
+    timetable: Timetable,
+    modifier: Modifier = Modifier,
+    content: @Composable (TimetableItem, Boolean) -> Unit,
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
+        items(timetable.contents, key = { it.timetableItem.id.value }) {
+            content(
+                it.timetableItem,
+                it.isFavorited
+            )
+        }
+    }
+}
