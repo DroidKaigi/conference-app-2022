9a5093652a62da0c96e7914be6702cee5c56f1b0
Takafumi Kanda <12084705+kafumi@users.noreply.github.com>
2022-09-06 00:38:05 +0900

    Apply spotless with updated configuration

--- core-zipline/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/zipline/SessionsZipline.kt
+++ core-zipline/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/zipline/SessionsZipline.kt
@@ -6,9 +6,6 @@ import app.cash.zipline.Zipline
 import app.cash.zipline.loader.ZiplineLoader
 import co.touchlab.kermit.Logger
 import io.github.droidkaigi.confsched2022.model.DroidKaigiSchedule
-import java.util.concurrent.Executors
-import javax.inject.Inject
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,6 +15,9 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
+import java.util.concurrent.Executors
+import javax.inject.Inject
+import kotlin.coroutines.EmptyCoroutineContext
 
 class SessionsZipline @Inject constructor(
     context: Application,
