900a3a4bb2dbd20a506743e433d8f7d1cdb5337b
adakoda <adakoda.jamjampan@gmail.com>
2022-09-06 01:53:16 +0900

    Scroll up when taps current tab #153
     - added a LazyListState to SessionList
     - added multiple LazyListState and pass them to SessionTopBar and SessionList
     - added jump to top on SessionsTopBar when user click selected tab

--- feature-sessions/src/main/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionList.kt
+++ feature-sessions/src/main/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionList.kt
@@ -3,6 +3,7 @@ package io.github.droidkaigi.confsched2022.feature.sessions
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,12 +14,14 @@ import io.github.droidkaigi.confsched2022.model.TimetableItem
 @Composable
 fun SessionList(
     timetable: Timetable,
+    listState: LazyListState,
     modifier: Modifier = Modifier,
     content: @Composable (TimetableItem, Boolean) -> Unit,
 ) {
     LazyColumn(
         modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(32.dp)
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+        state = listState
     ) {
         items(timetable.contents, key = { it.timetableItem.id.value }) {
             content(
