a9ab1530e95a2d32a44261d5acc9602b3908fd7e
kota-shiokara <kota.book.39@gmail.com>
2022-09-06 14:46:02 +0900

    fix: build error

--- feature-sessions/src/main/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionDetail.kt
+++ feature-sessions/src/main/java/io/github/droidkaigi/confsched2022/feature/sessions/SessionDetail.kt
@@ -1,8 +1,8 @@
 package io.github.droidkaigi.confsched2022.feature.sessions
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
