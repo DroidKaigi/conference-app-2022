42fa60b0c70b75603eae763ffa3b377d1347a1d7
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-09-06 11:09:22 +0900

    :recycle: Update Added test.

--- feature-about/src/test/java/io/github/droidkaigi/confsched2022/feature/about/AboutScreenRobot.kt
+++ feature-about/src/test/java/io/github/droidkaigi/confsched2022/feature/about/AboutScreenRobot.kt
@@ -1,6 +1,7 @@
 package io.github.droidkaigi.confsched2022.feature.about
 
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import io.github.droidkaigi.confsched2022.testing.RobotTestRule
 import javax.inject.Inject
 
@@ -8,6 +9,8 @@ class AboutScreenRobot @Inject constructor() {
 
     context(RobotTestRule)
     fun checkAboutVisible() {
+        composeTestRule.onNodeWithText("What is DroidKaigi?")
+        composeTestRule.onNodeWithText("DroidKaigiはエンジニアが主役のAndroidカンファレンスです。\\nAndroid技術情報の共有とコミュニケーションを目的に、2022年10月5日(水)〜7日(金)の3日間開催します。")
         composeTestRule.onNodeWithContentDescription("Twitter").assertExists()
         composeTestRule.onNodeWithContentDescription("YouTube").assertExists()
         composeTestRule.onNodeWithContentDescription("Medium").assertExists()
