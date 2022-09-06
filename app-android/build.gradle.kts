e8abbb281fb26089bd7a90c4c5e5e41d679ac701
Nthily <nthily@outlook.com>
2022-09-06 15:38:12 +0800

    add minifyEnabled for release

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -33,6 +33,13 @@ android {
         }
     }
     buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         debug {
             signingConfig = null
         }
