e840d5eca8b94ba33474d3fae339712fd6ca5c2a
ikemura23 <banbara.work@gmail.com>
2022-09-07 21:51:46 +0900

    feat: Moved CircularProgressIndicator to center

--- feature-sessions/src/main/java/io/github/droidkaigi/confsched2022/feature/sessions/Sessions.kt
+++ feature-sessions/src/main/java/io/github/droidkaigi/confsched2022/feature/sessions/Sessions.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -112,7 +113,12 @@ fun Sessions(
                 .padding(top = 4.dp)
         ) {
             if (scheduleState !is Loaded) {
-                CircularProgressIndicator()
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    CircularProgressIndicator()
+                }
             } else {
                 val days = scheduleState.schedule.days
                 if (isTimetable) {
