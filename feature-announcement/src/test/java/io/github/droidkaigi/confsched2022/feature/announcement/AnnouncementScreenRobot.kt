d0bc022a62a96eb1d16206f03b43c4129ab6e8b3
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-09-09 01:35:06 +0900

    :wrench: Fix Variable names were changed from temporary.

--- feature-announcement/src/test/java/io/github/droidkaigi/confsched2022/feature/announcement/AnnouncementScreenRobot.kt
+++ feature-announcement/src/test/java/io/github/droidkaigi/confsched2022/feature/announcement/AnnouncementScreenRobot.kt
@@ -11,8 +11,8 @@ class AnnouncementScreenRobot @Inject constructor() {
     context(RobotTestRule)
     fun checkAnnouncementVisible() {
         // TODO: Language-specific testing
-        val test = composeTestRule.activity.getString(string.announcement_top_app_bar_title)
-        composeTestRule.onNodeWithText(test).assertIsDisplayed()
+        val appBarTitleText = composeTestRule.activity.getString(string.announcement_top_app_bar_title)
+        composeTestRule.onNodeWithText(appBarTitleText).assertIsDisplayed()
     }
 
     operator fun invoke(
