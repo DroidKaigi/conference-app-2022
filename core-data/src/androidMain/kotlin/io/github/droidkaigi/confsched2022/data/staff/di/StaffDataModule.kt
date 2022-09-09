dcda3244d176867e298ca9e2d3c037b39343ad6f
t-nonomura <nono.develop@gmail.com>
2022-09-09 18:27:39 +0900

    Add staff repository

---
+++ core-data/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/data/staff/di/StaffDataModule.kt
@@ -0,0 +1,33 @@
+package io.github.droidkaigi.confsched2022.data.staff.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.droidkaigi.confsched2022.data.NetworkService
+import io.github.droidkaigi.confsched2022.data.contributors.ContributorsApi
+import io.github.droidkaigi.confsched2022.data.contributors.DataContributorsRepository
+import io.github.droidkaigi.confsched2022.data.staff.DataStaffRepository
+import io.github.droidkaigi.confsched2022.data.staff.StaffApi
+import io.github.droidkaigi.confsched2022.model.ContributorsRepository
+import io.github.droidkaigi.confsched2022.model.StaffRepository
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class StaffDataModule {
+
+    @Provides
+    @Singleton
+    fun provideStaffApi(networkService: NetworkService): StaffApi {
+        return StaffApi(networkService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideStaffRepository(
+        staffApi: StaffApi
+    ): StaffRepository {
+        return DataStaffRepository(staffApi)
+    }
+}
