fc1f108b606fee76bcb7f65e6e2273da0d8f667c
SSong-develop <thdgnsrl123@naver.com>
2022-09-09 23:11:29 +0900

    the indicator for loading contributors is located in the upper left corner

--- feature-contributors/src/main/java/io/github/droidkaigi/confsched2022/feature/contributors/Contributors.kt
+++ feature-contributors/src/main/java/io/github/droidkaigi/confsched2022/feature/contributors/Contributors.kt
@@ -6,8 +6,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -68,7 +72,12 @@ fun Contributors(
         }
     ) {
         if (uiModel.contributorsState !is Loaded) {
-            CircularProgressIndicator()
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
             return@KaigiScaffold
         }
         val contributors = uiModel.contributorsState.contributors
