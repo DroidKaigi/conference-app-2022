b6b1ab3ec87d5e051f7d8e6df71e1e2f169a29f1
t-nonomura <nono.develop@gmail.com>
2022-09-10 22:15:30 +0900

    Add staff screen preview

--- feature-staff/src/main/java/io/github/droidkaigi/confsched2022/feature/staff/Staff.kt
+++ feature-staff/src/main/java/io/github/droidkaigi/confsched2022/feature/staff/Staff.kt
@@ -22,11 +22,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiScaffold
+import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTheme
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTopAppBar
+import io.github.droidkaigi.confsched2022.model.Staff
+import io.github.droidkaigi.confsched2022.model.fakes
 import io.github.droidkaigi.confsched2022.ui.UiLoadState.Error
 import io.github.droidkaigi.confsched2022.ui.UiLoadState.Loading
 import io.github.droidkaigi.confsched2022.ui.UiLoadState.Success
@@ -112,5 +116,22 @@ fun Staff(
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StaffPreview() {
+    KaigiTheme {
+        Staff(
+            uiModel = StaffUiModel(
+                state = Success(
+                    Staff.fakes()
+                )
+            ),
+            showNavigationIcon = true,
+            onNavigationIconClick = {},
+            onLinkClick = { _, _ -> },
+        )
     }
 }
