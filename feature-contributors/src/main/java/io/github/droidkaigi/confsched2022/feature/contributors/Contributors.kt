89af427d922ffeeff0e0810091218c47ecc1d2e5
t-nonomura <nono.develop@gmail.com>
2022-09-11 17:52:15 +0900

    Created the UsernameRow as a common component

--- feature-contributors/src/main/java/io/github/droidkaigi/confsched2022/feature/contributors/Contributors.kt
+++ feature-contributors/src/main/java/io/github/droidkaigi/confsched2022/feature/contributors/Contributors.kt
@@ -1,17 +1,10 @@
 package io.github.droidkaigi.confsched2022.feature.contributors
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,13 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
+import io.github.droidkaigi.confsched2022.designsystem.components.UsernameRow
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiScaffold
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTheme
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTopAppBar
@@ -84,35 +74,12 @@ fun Contributors(
                     modifier = modifier.fillMaxWidth()
                 ) {
                     items(items = contributors, key = { it.id }) { contributor ->
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    contributor.profileUrl?.let { url ->
-                                        onLinkClick(url, "com.github.android")
-                                    }
-                                }
-                                .padding(vertical = 12.dp),
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Spacer(modifier = Modifier.width(16.dp))
-
-                            AsyncImage(
-                                model = contributor.iconUrl,
-                                contentDescription = contributor.username,
-                                alignment = Alignment.Center,
-                                contentScale = ContentScale.Fit,
-                                modifier = Modifier
-                                    .clip(CircleShape)
-                                    .size(60.dp)
-                            )
-
-                            Text(
-                                text = contributor.username,
-                                style = MaterialTheme.typography.titleMedium,
-                                modifier = Modifier.padding(start = 16.dp)
-                            )
-                        }
+                        UsernameRow(
+                            username = contributor.username,
+                            profileUrl = contributor.profileUrl,
+                            iconUrl = contributor.iconUrl,
+                            onLinkClick = onLinkClick
+                        )
                     }
                 }
             }
--- feature-staff/src/main/java/io/github/droidkaigi/confsched2022/feature/staff/Staff.kt
+++ feature-staff/src/main/java/io/github/droidkaigi/confsched2022/feature/staff/Staff.kt
@@ -1,17 +1,10 @@
 package io.github.droidkaigi.confsched2022.feature.staff
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,13 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
+import io.github.droidkaigi.confsched2022.designsystem.components.UsernameRow
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiScaffold
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTheme
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTopAppBar
@@ -84,34 +74,12 @@ fun Staff(
                     modifier = modifier.fillMaxWidth()
                 ) {
                     items(items = staff, key = { it.id }) { staff ->
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    staff.profileUrl?.let { url ->
-                                        onLinkClick(url, "com.github.android")
-                                    }
-                                },
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Spacer(modifier = Modifier.width(16.dp))
-
-                            AsyncImage(
-                                model = staff.iconUrl,
-                                contentDescription = staff.username,
-                                alignment = Alignment.Center,
-                                contentScale = ContentScale.Fit,
-                                modifier = Modifier
-                                    .clip(CircleShape)
-                                    .size(60.dp)
-                            )
-
-                            Text(
-                                text = staff.username,
-                                style = MaterialTheme.typography.titleMedium,
-                                modifier = Modifier.padding(start = 16.dp)
-                            )
-                        }
+                        UsernameRow(
+                            username = staff.username,
+                            profileUrl = staff.profileUrl,
+                            iconUrl = staff.iconUrl,
+                            onLinkClick = onLinkClick
+                        )
                     }
                 }
             }
