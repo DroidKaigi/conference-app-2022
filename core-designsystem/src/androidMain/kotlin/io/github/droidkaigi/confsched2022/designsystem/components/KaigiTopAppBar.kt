97ab933ed6fa69ce791b5f6c6f258756fd3ce6ce
tarumzu <tarumzu@gmail.com>
2022-09-12 23:58:14 +0900

    Use ProvideTextStyle in the title of KaigiTopAppBar

--- core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/components/KaigiTopAppBar.kt
+++ core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/components/KaigiTopAppBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.surfaceColorAtElevation
@@ -36,7 +37,9 @@ fun KaigiTopAppBar(
                 modifier = Modifier.fillMaxSize(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                title()
+                ProvideTextStyle(MaterialTheme.typography.titleLarge) {
+                    title()
+                }
                 Spacer(modifier = Modifier.weight(1F))
                 trailingIcons?.invoke(this)
             }
