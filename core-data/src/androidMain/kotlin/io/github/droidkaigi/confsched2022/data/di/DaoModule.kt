276c3181cd54aa5a437b710c3342a4393b93329e
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-09-13 11:08:14 +0900

    :wrench: Fix spotlessApply

--- core-data/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/data/di/DaoModule.kt
+++ core-data/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/data/di/DaoModule.kt
@@ -6,8 +6,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import io.github.droidkaigi.confsched2022.data.DriverFactory
 import io.github.droidkaigi.confsched2022.data.DatabaseService
+import io.github.droidkaigi.confsched2022.data.DriverFactory
 import io.github.droidkaigi.confsched2022.data.sessions.SessionsDao
 import javax.inject.Singleton
 
