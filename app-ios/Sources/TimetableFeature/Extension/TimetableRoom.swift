91f77806c3c24db5a863bd094158264d71e24590
ChikatoTakenouchi <maxmaxastea@gmail.com>
2022-09-14 08:18:36 +0900

    feat: create yellow color and edit hex

--- app-ios/Sources/TimetableFeature/Extension/TimetableRoom.swift
+++ app-ios/Sources/TimetableFeature/Extension/TimetableRoom.swift
@@ -7,8 +7,8 @@ extension TimetableRoom {
         switch name.enTitle {
         case "App Bar": return AssetColors.pink.swiftUIColor
         case "Backdrop": return AssetColors.orange.swiftUIColor
-        case "Cards": return AssetColors.blue.swiftUIColor
-        case "Dialogs": return AssetColors.purple.swiftUIColor
+        case "Cards": return AssetColors.yellow.swiftUIColor
+        case "Dialogs": return AssetColors.blue.swiftUIColor
         case "Online": return AssetColors.purple.swiftUIColor
         default: return AssetColors.pink.swiftUIColor
         }
