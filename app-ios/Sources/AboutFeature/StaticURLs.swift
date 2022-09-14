7a31b1da3147de9f8c45f9edfaaedfc60525a75e
NakaokaRei <reideeplearning@gmail.com>
2022-09-14 18:07:48 +0900

    feat: add StaticURLs

---
+++ app-ios/Sources/AboutFeature/StaticURLs.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public struct StaticURLs {
+    public static let twitter: String = "https://twitter.com/DroidKaigi"
+    public static let youtube: String = "https://www.youtube.com/c/DroidKaigi"
+    public static let medium: String = "https://medium.com/droidkaigi"
+}
