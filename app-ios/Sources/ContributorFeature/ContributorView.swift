00ef6be60f53095c006610b4e6d09ccb66dd4842
sudachi808 <sudachi808@gmail.com>
2022-09-15 08:30:32 +0900

    Revert "append contributor icon image"
    This reverts commit f17bacbb3e295e9250bd9f716868b1dafa3ec9c3.

--- app-ios/Sources/ContributorFeature/ContributorView.swift
+++ app-ios/Sources/ContributorFeature/ContributorView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 import Model
 import Assets
 import Theme
-import NukeUI
 
 public struct ContributorState: Equatable {
     public var contributors: [Contributor]
@@ -59,25 +58,24 @@ public struct ContributorView: View {
 
     public var body: some View {
         WithViewStore(store) { viewStore in
-            ScrollView {
-                LazyVStack(spacing: 0) {
-                    ForEach(viewStore.contributors, id: \.id) { contributor in
-                        Button(action: {
-                            guard let profileUrl = contributor.profileUrl else { return }
-                            guard let url = URL(string: profileUrl) else { return }
-                            if UIApplication.shared.canOpenURL(url) {
-                                UIApplication.shared.open(url)
-                            }
-                        }, label: {
-                            ContributorItemView(contributor: contributor)
-                        })
-                    }
+            NavigationView {
+                List(viewStore.contributors, id: \.id) { contributor in
+                    Button(action: {
+                        guard let profileUrl = contributor.profileUrl else { return }
+                        guard let url = URL(string: profileUrl) else { return }
+                        if UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.open(url)
+                        }
+                    }, label: {
+                        ContributorItemView(contributor: contributor)
+                    })
+                    .hideListRowSeparator()
                 }
-                .task {
-                    await viewStore.send(.refresh).finish()
-                }
+                .listStyle(PlainListStyle())
             }
-            .background(AssetColors.background.swiftUIColor)
+            .task {
+                await viewStore.send(.refresh).finish()
+            }
         }
     }
 }
@@ -87,17 +85,13 @@ struct ContributorItemView: View {
     let contributor: Contributor
 
     var body: some View {
-        HStack(alignment: .center, spacing: 16) {
-            LazyImage(url: URL(string: contributor.iconUrl))
+        HStack(spacing: 16) {
+            Assets.colorfulLogo.swiftUIImage
                 .frame(width: 60, height: 60)
-                .clipShape(Circle())
             Text(contributor.username)
                 .font(Font.system(size: 16, weight: .bold, design: .default))
                 .foregroundColor(AssetColors.onBackground.swiftUIColor)
-            Spacer()
         }
-        .padding(.horizontal, 16)
-        .padding(.vertical, 12)
     }
 }
 
@@ -131,6 +125,15 @@ struct ContributorView_Previews: PreviewProvider {
                 )
             )
         )
+        .preferredColorScheme(.dark)
+    }
+}
+struct ContributorViewItem_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContributorItemView(contributor: Contributor.companion.fakes().first!)
+        }
+        .previewLayout(.fixed(width: 390, height: 60 + 8))
         .preferredColorScheme(.dark)
     }
 }
