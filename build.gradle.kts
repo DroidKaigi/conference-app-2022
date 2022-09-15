f8daeff7d48dfc71c93381bbb82a71a074cd8e5a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-09-16 01:40:30 +0900

    Suppress warnings on libs in build.gradle.kts (DroidKaigi2022)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.androidGradlePlugin) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.androidGradleLibraryPlugin) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.kotlinPlugin) apply false
     id("droidkaigi.primitive.dependencygraph")
 }
