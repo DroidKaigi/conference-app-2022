231a5830dad6932487a777594b8f438119b84873
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-09-16 23:10:02 +0900

    Add comment to tell readers why we need @Suppress

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    // https://youtrack.jetbrains.com/issue/KTIJ-19369/False-positive-cant-be-called-in-this-context-by-implicit-receiver-with-plugins-in-Gradle-version-catalogs-as-a-TOML-file#focus=Comments-27-6204464.0-0
     @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.androidGradlePlugin) apply false
     @Suppress("DSL_SCOPE_VIOLATION")
