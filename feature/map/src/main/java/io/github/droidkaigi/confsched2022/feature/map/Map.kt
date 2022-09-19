356db47013f0da1536a8fff83cb275e0161ede64
Masatoshi Kubode <swi.masatoshi@gmail.com>
2022-09-19 22:39:27 +0900

    Implement bottom app bar on Map

--- feature/map/src/main/java/io/github/droidkaigi/confsched2022/feature/map/Map.kt
+++ feature/map/src/main/java/io/github/droidkaigi/confsched2022/feature/map/Map.kt
@@ -1,17 +1,26 @@
 package io.github.droidkaigi.confsched2022.feature.map
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import co.touchlab.kermit.Logger
 import dev.icerock.moko.resources.compose.stringResource
 import io.github.droidkaigi.confsched2022.designsystem.components.KaigiScaffold
 import io.github.droidkaigi.confsched2022.designsystem.components.KaigiTopAppBar
+import io.github.droidkaigi.confsched2022.feature.map.R.drawable
 import io.github.droidkaigi.confsched2022.strings.Strings
 
 // FIXME: This is a temporary image.
@@ -22,10 +31,12 @@ private const val MAP_IMAGE_URL = "https://user-images.githubusercontent.com" +
 fun MapScreenRoot(
     showNavigationIcon: Boolean,
     onNavigationIconClick: () -> Unit,
+    onPinClick: () -> Unit = { Logger.w("TODO: Pin is not implemented yet!") },
 ) {
     Map(
         showNavigationIcon = showNavigationIcon,
         onNavigationIconClick = onNavigationIconClick,
+        onPinClick = onPinClick,
     )
 }
 
@@ -34,6 +45,7 @@ fun Map(
     modifier: Modifier = Modifier,
     showNavigationIcon: Boolean,
     onNavigationIconClick: () -> Unit,
+    onPinClick: () -> Unit,
 ) {
     KaigiScaffold(
         modifier = modifier,
@@ -47,7 +59,12 @@ fun Map(
                     )
                 },
             )
-        }
+        },
+        bottomBar = {
+            MapBottomAppBar(
+                onPinClick = onPinClick,
+            )
+        },
     ) { innerPadding ->
         Box(
             modifier = Modifier
@@ -60,6 +77,29 @@ fun Map(
                 text = "Map is temporarily unavailable :)",
                 color = MaterialTheme.colorScheme.onBackground
             )
+        }
+    }
+}
+
+@Composable
+fun MapBottomAppBar(
+    onPinClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    BottomAppBar(modifier = modifier) {
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                IconButton(onClick = { onPinClick() }) {
+                    Icon(
+                        painter = painterResource(id = drawable.ic_pin),
+                        contentDescription = "pin",
+                    )
+                }
+            }
         }
     }
 }
