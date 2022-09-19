1f23892698b7214f5f36fb1fe0d2b202ab524097
misuzz <fs9dm3cc8ct1@gmail.com>
2022-09-19 21:18:30 +0900

    add KaigiTag preview function

--- core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/components/KaigiTag.kt
+++ core-designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/components/KaigiTag.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -16,7 +17,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTheme
 
 @Composable
 fun KaigiTag(
@@ -39,6 +42,16 @@ fun KaigiTag(
             LocalTextStyle provides labelTextStyle,
         ) {
             label()
+        }
+    }
+}
+
+@Preview
+@Composable
+public fun KaigiTagPreview() {
+    KaigiTheme {
+        KaigiTag(backgroundColor = MaterialTheme.colorScheme.secondaryContainer) {
+            Text(text = "sample")
         }
     }
 }
