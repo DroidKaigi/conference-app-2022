42cbe31db51fed6a6852c20c100dd7a9a4663b3c
higekick <higekick39@gmail.com>
2022-09-20 00:13:36 +0900

    add detekt to designsystem

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("droidkaigi.convention.kmp")
     id("droidkaigi.primitive.android.compose")
+    id("droidkaigi.primitive.detekt")
 }
 
 android.namespace = "io.github.droidkaigi.confsched2022.core.designsystem"
