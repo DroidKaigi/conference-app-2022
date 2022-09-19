74e0be7258d087c982fc395ef31355f6e80d2390
Masatoshi Kubode <swi.masatoshi@gmail.com>
2022-09-19 22:22:58 +0900

    Implement navigation

--- feature/map/src/main/java/io/github/droidkaigi/confsched2022/feature/map/Map.kt
+++ feature/map/src/main/java/io/github/droidkaigi/confsched2022/feature/map/Map.kt
@@ -3,32 +3,63 @@ package io.github.droidkaigi.confsched2022.feature.map
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import dev.icerock.moko.resources.compose.stringResource
+import io.github.droidkaigi.confsched2022.designsystem.components.KaigiScaffold
+import io.github.droidkaigi.confsched2022.designsystem.components.KaigiTopAppBar
+import io.github.droidkaigi.confsched2022.strings.Strings
 
 // FIXME: This is a temporary image.
 private const val MAP_IMAGE_URL = "https://user-images.githubusercontent.com" +
     "/5885032/191023087-d595c963-31b9-45e6-be9c-55565192b76e.png"
 
 @Composable
-fun MapScreenRoot() {
-    Map()
+fun MapScreenRoot(
+    showNavigationIcon: Boolean,
+    onNavigationIconClick: () -> Unit,
+) {
+    Map(
+        showNavigationIcon = showNavigationIcon,
+        onNavigationIconClick = onNavigationIconClick,
+    )
 }
 
 @Composable
 fun Map(
     modifier: Modifier = Modifier,
+    showNavigationIcon: Boolean,
+    onNavigationIconClick: () -> Unit,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Map is temporarily unavailable :)",
-            color = MaterialTheme.colorScheme.onBackground
-        )
+    KaigiScaffold(
+        modifier = modifier,
+        topBar = {
+            KaigiTopAppBar(
+                showNavigationIcon = showNavigationIcon,
+                onNavigationIconClick = onNavigationIconClick,
+                title = {
+                    Text(
+                        text = stringResource(Strings.map_top_app_bar_title),
+                    )
+                },
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Map is temporarily unavailable :)",
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
     }
 }
