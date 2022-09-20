6586f812fd7e1befb7a38bd71c8530b62f02d968
ryoya ito <ry.itto.github@gmail.com>
2022-09-21 02:23:49 +0900

    stop ios 15 support

--- app-ios/Package.swift
+++ app-ios/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ var package = Package(
     name: "DroidKaigiPackage",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v16),
     ],
     products: [
         .library(name: "AboutFeature", targets: ["AboutFeature"]),
