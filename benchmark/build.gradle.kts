38a1e31dc1fb85390682f1467a59a713aa739433
hiraok <hiraok.work@gmail.com>
2022-09-21 10:09:25 +0900

    Add benchmark module and StartupBenchmark.kt

---
+++ benchmark/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.test")
+    id("org.jetbrains.kotlin.android")
+}
+
+@Suppress("UnstableApiUsage")
+android {
+    namespace = "io.github.droidkaigi.confsched2022.benchmark"
+    compileSdk = 33
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    defaultConfig {
+        minSdk = 28
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        missingDimensionStrategy("network", "prod")
+    }
+
+    buildTypes {
+        // This benchmark buildType is used for benchmarking, and should function like your
+        // release build (for example, with minification on). It"s signed with a debug key
+        // for easy local/CI testing.
+        create("benchmark") {
+            isDebuggable = true
+            signingConfig = getByName("debug").signingConfig
+            matchingFallbacks += listOf("release")
+        }
+    }
+
+    targetProjectPath = ":app-android"
+    experimentalProperties["android.experimental.self-instrumenting"] = true
+}
+
+dependencies {
+    implementation(libs.androidxTestExtJunit)
+    implementation(libs.androidxTestEspressoEspressoCore)
+    implementation(libs.androidxTestUiAutomator)
+    implementation(libs.androidxMacroBenchmark)
+}
+
+androidComponents {
+    beforeVariants(selector().all()) {
+        it.enable = it.buildType == "benchmark"
+    }
+}
