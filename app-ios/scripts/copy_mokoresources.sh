0b20d6fe0c8d503d52ece7bb71f3d02477abddbf
ryoya ito <ry.itto.github@gmail.com>
2022-09-23 01:44:29 +0900

    wrap with ""

--- app-ios/scripts/copy_mokoresources.sh
+++ app-ios/scripts/copy_mokoresources.sh
@@ -26,7 +26,7 @@ else
 fi
 
 "$SRCROOT/../../gradlew" -p "$SRCROOT/../../" :appioscombined:copyFrameworkResourcesToApp \
-    -Pmoko.resources.PLATFORM_NAME=$PLATFORM_NAME \
-    -Pmoko.resources.CONFIGURATION=$CONFIGURATION \
-    -Pmoko.resources.BUILT_PRODUCTS_DIR=$BUILT_PRODUCTS_DIR \
-    -Pmoko.resources.CONTENTS_FOLDER_PATH=$CONTENTS_FOLDER_PATH
+    -Pmoko.resources.PLATFORM_NAME="$PLATFORM_NAME" \
+    -Pmoko.resources.CONFIGURATION="$CONFIGURATION" \
+    -Pmoko.resources.BUILT_PRODUCTS_DIR="$BUILT_PRODUCTS_DIR" \
+    -Pmoko.resources.CONTENTS_FOLDER_PATH="$CONTENTS_FOLDER_PATH"
