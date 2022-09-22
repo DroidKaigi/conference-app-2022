bd826ede81bb5dea826ff75bc9a568bfcd44c132
ryoya ito <ry.itto.github@gmail.com>
2022-09-23 01:18:42 +0900

    fix for XcodeCloud's env value "CI"

--- app-ios/scripts/copy_mokoresources.sh
+++ app-ios/scripts/copy_mokoresources.sh
@@ -17,7 +17,7 @@ function prop {
     grep "${1}" ${file} | cut -d'=' -f2
 }
 
-if [ "$CI" = true ] ; then
+if [ "$CI" = true -o "$CI" = TRUE ] ; then
     export PATH="/usr/local/opt/openjdk/bin:$PATH"
 else
     export JAVA_HOME=$(prop 'org.gradle.java.home')
