d0560a33533ff5181a27ead480c5d42a0afb35c5
takahirom <takam.dev@gmail.com>
2022-09-23 23:42:04 +0900

    Format

--- core/designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/components/KaigiScaffold.kt
+++ core/designsystem/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/designsystem/components/KaigiScaffold.kt
@@ -2,13 +2,8 @@ package io.github.droidkaigi.confsched2022.designsystem.components
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarData
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.droidkaigi.confsched2022.designsystem.theme.KaigiTheme
