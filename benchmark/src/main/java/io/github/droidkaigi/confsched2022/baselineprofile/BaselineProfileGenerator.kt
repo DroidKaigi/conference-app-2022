644f5ab7737e30deae1515c1ecf206850e810eae
hiraok <hiraok.work@gmail.com>
2022-09-24 20:23:20 +0900

    Add BaselineProfileGenerator.kt

---
+++ benchmark/src/main/java/io/github/droidkaigi/confsched2022/baselineprofile/BaselineProfileGenerator.kt
@@ -0,0 +1,31 @@
+package io.github.droidkaigi.confsched2022.baselineprofile
+
+import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.By
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalBaselineProfilesApi
+@RunWith(AndroidJUnit4::class)
+class BaselineProfileGenerator {
+
+    @get:Rule val baselineProfileRule = BaselineProfileRule()
+
+    @Test
+    fun startup() =
+        baselineProfileRule.collectBaselineProfile(
+            packageName = "io.github.droidkaigi.confsched2022.dev"
+        ) {
+            pressHome()
+            startActivityAndWait()
+            device.waitForIdle()
+
+            device.run {
+               device.findObject(By.res("sessionDayTab")).click()
+               waitForIdle()
+            }
+        }
+}
