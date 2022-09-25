2b9709cb78d4310ead73d948dd8a325dbf81bf91
ryoya ito <ry.itto.github@gmail.com>
2022-09-26 00:08:23 +0900

    copy plist

--- app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
+++ app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,3 +4,5 @@ export PATH="/usr/local/opt/openjdk/bin:$PATH"
 
 cd "$CI_WORKSPACE/app-ios"
 ./scripts/build_kmm_framework.sh Release
+
+base64 -d $GOOGLE_INFO_PLIST > DroidKaigi2022/Shared/GoogleService-Info.plist
--- app-ios/scripts/build_kmm_framework.sh
+++ app-ios/scripts/build_kmm_framework.sh
@@ -6,4 +6,5 @@ CONFIGURATION=${1}
 cd "$(dirname $SCRIPT_PATH)/../../"
 ./gradlew appioscombined:assembleXCFramework
 
+mkdir -p "app-ios/build"
 cp -r "app-ios-combined/build/XCFrameworks/$CONFIGURATION/appioscombined.xcframework" "app-ios/build"
