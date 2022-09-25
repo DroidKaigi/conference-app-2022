7827aeb521f6826435a6122a9e981bc322bdc7b0
ryoya ito <ry.itto.github@gmail.com>
2022-09-26 02:23:23 +0900

    fix

--- app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
+++ app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
@@ -5,4 +5,4 @@ export PATH="/usr/local/opt/openjdk/bin:$PATH"
 cd "$CI_WORKSPACE/app-ios"
 ./scripts/build_kmm_framework.sh Release
 
-base64 -d $GOOGLE_INFO_PLIST > DroidKaigi2022/Shared/GoogleService-Info.plist
+echo $GOOGLE_INFO_PLIST | base64 -d > DroidKaigi2022/Shared/GoogleService-Info.plist
