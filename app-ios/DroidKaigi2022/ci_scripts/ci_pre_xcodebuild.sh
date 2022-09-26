1f083389739d3954b61f477f906950ccf5983cab
ryoya ito <ry.itto.github@gmail.com>
2022-09-26 16:01:28 +0900

    once use debug info plist

--- app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
+++ app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
@@ -5,4 +5,4 @@ export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
 cd "$CI_WORKSPACE/app-ios"
 ./scripts/build_kmm_framework.sh Release
 
-echo $GOOGLE_INFO_PLIST | base64 -d > DroidKaigi2022/Shared/GoogleService-Info.plist
+#echo $GOOGLE_INFO_PLIST | base64 -d > DroidKaigi2022/Shared/GoogleService-Info.plist
