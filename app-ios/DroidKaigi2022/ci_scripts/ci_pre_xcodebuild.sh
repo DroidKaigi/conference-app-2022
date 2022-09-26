5007611df420c1bf1fc9bb587470d5e0f12baa84
ryoya ito <ry.itto.github@gmail.com>
2022-09-26 19:04:42 +0900

    add prod specification

--- app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
+++ app-ios/DroidKaigi2022/ci_scripts/ci_pre_xcodebuild.sh
@@ -3,6 +3,6 @@
 export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
 
 cd "$CI_WORKSPACE/app-ios"
-./scripts/build_kmm_framework.sh Release
+./scripts/build_kmm_framework.sh release prod
 
 echo $GOOGLE_INFO_PLIST | base64 -d > DroidKaigi2022/Shared/GoogleService-Info.plist
