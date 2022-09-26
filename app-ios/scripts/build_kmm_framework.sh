7242f0eb5b22a7cde37d25bd18d7fd9e24451384
ryoya ito <ry.itto.github@gmail.com>
2022-09-26 18:40:14 +0900

    fix prod framework

--- app-ios/scripts/build_kmm_framework.sh
+++ app-ios/scripts/build_kmm_framework.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Usage: ./build_kmm_framework.sh BUILD(debug/release) CONFIGURATION(dev/prod)
+
 SCRIPT_PATH=${0}
-CONFIGURATION=${1}
+BUILD=${1}
+FLAVOR=${2}
 
 cd "$(dirname $SCRIPT_PATH)/../../"
-./gradlew appioscombined:assembleXCFramework
+./gradlew appioscombined:assembleXCFramework -Pbuildkonfig.flavor=$FLAVOR
 
 mkdir -p "app-ios/build"
-cp -r "app-ios-combined/build/XCFrameworks/$CONFIGURATION/appioscombined.xcframework" "app-ios/build"
+cp -r "app-ios-combined/build/XCFrameworks/$BUILD/appioscombined.xcframework" "app-ios/build"
