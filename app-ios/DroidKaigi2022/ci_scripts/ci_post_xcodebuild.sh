d5865a54135eed22deca3a2adb1d7030d9f6bab8
ryoya ito <ry.itto.github@gmail.com>
2022-09-30 03:08:45 +0900

    fix platform name

--- app-ios/DroidKaigi2022/ci_scripts/ci_post_xcodebuild.sh
+++ app-ios/DroidKaigi2022/ci_scripts/ci_post_xcodebuild.sh
@@ -4,5 +4,5 @@
 cd "$CI_WORKSPACE/app-ios"
 ./scripts/upload-symbols \
     -gsp "DroidKaigi2022/Shared/GoogleService-Info.plist" \
-    -p "$CI_PRODUCT_PLATFORM" \
+    -p "ios" \
     "$CI_ARCHIVE_PATH/dSYMs/$CI_PRODUCT.app.dSYM"
