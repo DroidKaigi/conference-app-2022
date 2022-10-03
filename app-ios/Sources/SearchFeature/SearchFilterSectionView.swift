5ea754653507c4369af3f4b237ca0951e5426eff
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-10-04 02:43:08 +0900

    Update SearchFiltersSectionView

--- app-ios/Sources/SearchFeature/SearchFilterSectionView.swift
+++ app-ios/Sources/SearchFeature/SearchFilterSectionView.swift
@@ -5,22 +5,36 @@ import SwiftUI
 
 struct SearchFiltersSectionView: View {
     private let store: Store<SearchState, SearchAction>
-    init(store: Store<SearchState, SearchAction>) {
+
+    struct ViewState: Equatable {
+        public var filterFavorite: Bool
+        public var filterDays: [DroidKaigi2022Day]
+        public var filterCategories: [TimetableCategory]
+
+        public init(state: SearchState) {
+            self.filterFavorite = state.filterFavorite
+            self.filterDays = state.filterDays
+            self.filterCategories = state.filterCategories
+        }
+    }
+
+    public init(store: Store<SearchState, SearchAction>) {
         self.store = store
     }
-    var body: some View {
-        WithViewStore(store) { viewStore in
+
+    public var body: some View {
+        WithViewStore(store.scope(state: ViewState.init)) { viewStore in
             ScrollView(.horizontal) {
                 HStack(spacing: 8) {
                     FilterButtonView(
-                        title: viewStore.filterDay?.name ?? StringsKt.shared.search_filter_select_day.localized(),
-                        isFiltered: viewStore.filterDay != nil
+                        title: viewStore.filterDays.isEmpty ?
+                            StringsKt.shared.search_filter_select_day.localized() : viewStore.filterDays.map { $0.name }.joined(separator: ","),
+                        isFiltered: !viewStore.filterDays.isEmpty
                     ) {
                         viewStore.send(.showDayFilterSheet)
                     }
                     FilterButtonView(
-                        title: viewStore.filterCategories.isEmpty ? StringsKt.shared.search_filter_select_category
-                            .localized(): viewStore.filterCategories.map { $0.title.currentLangTitle }.joined(separator: ","),
+                        title: viewStore.filterCategories.isEmpty ? StringsKt.shared.search_filter_select_category.localized() : viewStore.filterCategories.map { $0.title.currentLangTitle }.joined(separator: ","),
                         isFiltered: !viewStore.filterCategories.isEmpty
                     ) {
                         viewStore.send(.showCategoryFilterSheet)
