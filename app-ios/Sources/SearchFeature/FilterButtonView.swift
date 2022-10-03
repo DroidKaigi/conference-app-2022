b4a7e30811ae1958e42a732fa9400c060717e9b2
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-10-04 02:42:36 +0900

    Update FilterButtonView

--- app-ios/Sources/SearchFeature/FilterButtonView.swift
+++ app-ios/Sources/SearchFeature/FilterButtonView.swift
@@ -4,7 +4,7 @@ import Theme
 struct FilterButtonView: View {
     let title: String
     let isFiltered: Bool
-    var onTap: () -> Void
+    let onTap: () -> Void
     var body: some View {
         Button {
             onTap()
@@ -13,25 +13,25 @@ struct FilterButtonView: View {
                 if isFiltered {
                     Image(systemName: "checkmark")
                         .resizable()
-                        .frame(width: 8, height: 8)
-                        .foregroundColor(isFiltered ? AssetColors.onSurface.swiftUIColor : AssetColors.white.swiftUIColor)
+                        .frame(width: 12, height: 12)
+                        .foregroundColor(isFiltered ? AssetColors.onSurface.swiftUIColor : AssetColors.onSurfaceVariant.swiftUIColor)
                 }
                 Text(title)
-                    .foregroundColor(isFiltered ? AssetColors.onSurface.swiftUIColor : AssetColors.white.swiftUIColor)
+                    .foregroundColor(isFiltered ? AssetColors.onSurface.swiftUIColor : AssetColors.onSurfaceVariant.swiftUIColor)
                     .font(.system(size: 14, weight: .semibold))
                 Image(systemName: "arrowtriangle.down.fill")
                     .resizable()
-                    .frame(width: 4, height: 4)
-                    .foregroundColor(isFiltered ? AssetColors.onSurface.swiftUIColor : AssetColors.white.swiftUIColor)
+                    .frame(width: 8, height: 8)
+                    .foregroundColor(isFiltered ? AssetColors.onSurface.swiftUIColor : AssetColors.onSurfaceVariant.swiftUIColor)
             }
             .animation(.linear(duration: 0.2), value: isFiltered)
-            .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 20))
+            .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
             .background(isFiltered ? AssetColors.secondaryContainer.swiftUIColor : AssetColors.background.swiftUIColor
             )
             .cornerRadius(8)
             .overlay(
                 RoundedRectangle(cornerRadius: 8)
-                    .stroke(isFiltered ? AssetColors.secondaryContainer.swiftUIColor : AssetColors.white.swiftUIColor, lineWidth: 1)
+                    .stroke(isFiltered ? AssetColors.secondaryContainer.swiftUIColor : AssetColors.outline.swiftUIColor, lineWidth: 1)
             )
         }
     }
