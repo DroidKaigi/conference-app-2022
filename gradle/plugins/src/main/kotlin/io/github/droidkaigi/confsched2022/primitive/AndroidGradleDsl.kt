95bb76437a515eb1112de33f2fed230b3a6576aa
takahirom <takam.dev@gmail.com>
2022-10-04 21:15:21 +0900

    Update android version

--- gradle/plugins/src/main/kotlin/io/github/droidkaigi/confsched2022/primitive/AndroidGradleDsl.kt
+++ gradle/plugins/src/main/kotlin/io/github/droidkaigi/confsched2022/primitive/AndroidGradleDsl.kt
@@ -29,8 +29,8 @@ fun Project.setupAndroid() {
         defaultConfig {
             minSdk = 23
 
-            versionCode = 5
-            versionName = "1.2.0"
+            versionCode = 6
+            versionName = "1.3.0"
         }
 
         compileOptions {
