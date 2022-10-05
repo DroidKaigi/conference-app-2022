05faebb4d85c325ba48c0c8be0bb3f52af6f42ce
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-10-05 21:06:47 +0900

    Session title reflects application settings

--- core/model/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/model/Locale.kt
+++ core/model/src/androidMain/kotlin/io/github/droidkaigi/confsched2022/model/Locale.kt
@@ -1,8 +1,21 @@
 package io.github.droidkaigi.confsched2022.model
 
-public actual fun getDefaultLocale(): Locale =
-    if (java.util.Locale.getDefault() == java.util.Locale.JAPAN) {
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
+
+public actual fun getDefaultLocale(): Locale {
+    val applicationLocales = AppCompatDelegate.getApplicationLocales()
+    if (!applicationLocales.isEmpty) {
+        return if (applicationLocales == LocaleListCompat.forLanguageTags("ja")) {
+            Locale.JAPAN
+        } else {
+            Locale.OTHER
+        }
+    }
+
+    return if (java.util.Locale.getDefault() == java.util.Locale.JAPAN) {
         Locale.JAPAN
     } else {
         Locale.OTHER
     }
+}
