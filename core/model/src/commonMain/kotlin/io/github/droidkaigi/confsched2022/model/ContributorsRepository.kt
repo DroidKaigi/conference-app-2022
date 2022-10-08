6b395e4a541e15f4a10029ad557e34d994e2e929
woxtu <woxtup@gmail.com>
2022-10-09 05:55:45 +0900

    Specify exceptions that may occur

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2022/model/ContributorsRepository.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2022/model/ContributorsRepository.kt
@@ -2,8 +2,10 @@ package io.github.droidkaigi.confsched2022.model
 
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.coroutines.flow.Flow
+import kotlin.coroutines.cancellation.CancellationException
 
 public interface ContributorsRepository {
     public fun contributors(): Flow<PersistentList<Contributor>>
+    @Throws(AppError::class, CancellationException::class)
     public suspend fun refresh()
 }
--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2022/model/StaffRepository.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2022/model/StaffRepository.kt
@@ -2,9 +2,10 @@ package io.github.droidkaigi.confsched2022.model
 
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.coroutines.flow.Flow
+import kotlin.coroutines.cancellation.CancellationException
 
 public interface StaffRepository {
     public fun staff(): Flow<PersistentList<Staff>>
-
+    @Throws(AppError::class, CancellationException::class)
     public suspend fun refresh()
 }
